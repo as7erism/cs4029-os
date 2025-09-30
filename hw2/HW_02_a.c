62f647f8324e15d7c8dd813b3d280580d5b358af
aster <astermayhew@proton.me>
2025-09-29 23:23:32 -0400

    reword comment

--- hw2/HW_02_a.c
+++ hw2/HW_02_a.c
@@ -2,8 +2,8 @@
 
 int main(int argc, char** argv) {
   // argv is an array (pointer) of strings (arrays of chars) whose last element
-  // is the empty string. we can pass the string pointed to by argv to printf,
-  // incrememnting the pointer each time, until weve reached the empty string.
+  // is the empty string. until weve reached the empty string, we can pass the
+  // string pointed to by argv to printf, incrememnting the pointer each time.
   while (*argv != 0)
     printf("%s\n", *(argv++));
 }
